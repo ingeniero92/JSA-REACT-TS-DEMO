59ec631324868fcf4ab7eea39a3e1b9c1a892461
José Serrano Álvarez <ingeniero92@gmail.com>
2023-05-30 12:17:53 +0200

    Pollyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/jsa-react-ts-demo/",
-    plugins: [react()],
+    plugins: [react(), nodePolyfills()],
 })
+
