d4d29855b288db4d47e19e7077a8cbcdcf228721
José Serrano Álvarez <ingeniero92@gmail.com>
2023-05-30 13:15:24 +0200

    Legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import react from "@vitejs/plugin-react";
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/jsa-react-ts-demo/",
-    plugins: [react()],
+    plugins: [legacy()],
 })
