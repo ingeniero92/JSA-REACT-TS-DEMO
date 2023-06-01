babd88a9d83166ef09c68cc6947f381d5a383936
José Serrano Álvarez <ingeniero92@gmail.com>
2023-06-01 12:25:36 +0200

    Danger Warning
    Design under construction

--- src/components/Header/TitleComponent.tsx
+++ src/components/Header/TitleComponent.tsx
@@ -1,17 +1,27 @@
+import { useUiStore } from "../../hooks";
+
 import JSA from "../../assets/img/JSA.jpg";
 
 import styles from "../../styles/modules/header.module.css";
 
 export const TitleComponent = () => {
+
+	const { t } = useUiStore();
+
 	return (
 		<div className={ styles.title }>
-			<div>
+			<div className={ styles.imgContainer }>
 				<img src={ JSA } />
 			</div>        
-			<span>
+			<span className={ styles.name } >
 				<i className="fa-brands fa-react"/> 
 				{ import.meta.env.VITE_APP_TITLE } 
 				<i className="fa-brands fa-react"/>
+			</span>
+			<span className={ styles.danger } >
+				<i className="fa-solid fa-circle-exclamation"/>
+				{ t("header-danger")}
+				<i className="fa-solid fa-circle-exclamation"/>
 			</span>
 		</div>
 	)
